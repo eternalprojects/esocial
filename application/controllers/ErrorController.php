3efcb4b01af1ba24f9b56c517ec12b40f624c58c
jplesperance <jplesperance@453734df-fbbb-4f4f-9a73-a08973522899>
2011-07-21 05:57:37 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/esocial/trunk@5 453734df-fbbb-4f4f-9a73-a08973522899

--- application/controllers/ErrorController.php
+++ application/controllers/ErrorController.php
@@ -7,7 +7,7 @@ class ErrorController extends Zend_Controller_Action
     {
         $errors = $this->_getParam('error_handler');
         
-        if (!$errors || !$errors instanceof ArrayObject) {
+        if (!$errors) {
             $this->view->message = 'You have reached the error page';
             return;
         }
@@ -16,23 +16,21 @@ class ErrorController extends Zend_Controller_Action
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ROUTE:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+        
                 // 404 error -- controller or action not found
                 $this->getResponse()->setHttpResponseCode(404);
-                $priority = Zend_Log::NOTICE;
                 $this->view->message = 'Page not found';
                 break;
             default:
                 // application error
                 $this->getResponse()->setHttpResponseCode(500);
-                $priority = Zend_Log::CRIT;
                 $this->view->message = 'Application error';
                 break;
         }
         
         // Log exception, if logger available
         if ($log = $this->getLog()) {
-            $log->log($this->view->message, $priority, $errors->exception);
-            $log->log('Request Parameters', $priority, $errors->request->getParams());
+            $log->crit($this->view->message, $errors->exception);
         }
         
         // conditionally display exceptions
--- docs/README.txt
+++ docs/README.txt
@@ -2,7 +2,7 @@ README
 ======
 
 This directory should be used to place project specfic documentation including
-but not limited to project notes, generated API/phpdoc documentation, or
+but not limited to project notes, generated API/phpdoc documentation, or 
 manual files generated or hand written.  Ideally, this directory would remain
 in your development environment only and should not be deployed with your
 application to it's final production location.
@@ -14,17 +14,17 @@ Setting Up Your VHOST
 The following is a sample VHOST you might want to consider for your project.
 
 <VirtualHost *:80>
-   DocumentRoot "/var/www/esocial/trunk/public"
-   ServerName trunk.local
+   DocumentRoot "C:/Program Files (x86)/Zend/Apache2/htdocs/eSocial/public"
+   ServerName .local
 
    # This should be omitted in the production environment
    SetEnv APPLICATION_ENV development
-
-   <Directory "/var/www/esocial/trunk/public">
+    
+   <Directory "C:/Program Files (x86)/Zend/Apache2/htdocs/eSocial/public">
        Options Indexes MultiViews FollowSymLinks
        AllowOverride All
        Order allow,deny
        Allow from all
    </Directory>
-
+    
 </VirtualHost>
