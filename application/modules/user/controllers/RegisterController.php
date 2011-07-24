9bedb80ef5198971d0acb175295106cf47a867b0
jplesperance <jplesperance@453734df-fbbb-4f4f-9a73-a08973522899>
2011-07-24 05:14:50 +0000

    SFSOCIAL-7
    Generated a basic view for the registration form
    
    git-svn-id: https://svn.codespaces.com/jplwebinnov/esocial/trunk@14 453734df-fbbb-4f4f-9a73-a08973522899

---
+++ application/modules/user/controllers/RegisterController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * RegisterController
+ * 
+ * @author
+ * @version 
+ */
+require_once 'Zend/Controller/Action.php';
+class RegisterController extends Zend_Controller_Action
+{
+    /**
+     * The default action - show the home page
+     */
+    public function indexAction ()
+    {
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/modules/user/forms/register.ini','register');
+        $form = new Zend_Form($config->register);
+        if($this->getRequest()->isPost()){
+            if($form->isValid($this->getRequest()->getPost())){
+                
+            }else{
+                $this->view->form = $form;
+            }
+        }
+    }
+}
