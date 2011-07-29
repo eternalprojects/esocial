37cb67957de4a0d2a9b5234a91c5c071e498fdcd
jplesperance <jplesperance@453734df-fbbb-4f4f-9a73-a08973522899>
2011-07-29 21:25:40 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/esocial/trunk@76 453734df-fbbb-4f4f-9a73-a08973522899

--- tests/application/modules/user/controllers/RegisterControllerTest.php
+++ tests/application/modules/user/controllers/RegisterControllerTest.php
@@ -97,6 +97,56 @@ class User_RegisterControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
         $this->assertAction($urlParams['action']);
         $this->assertNotRedirect();
     }
+    
+    public function testDuplicateUsername(){
+    	$this->_insertTestUser();
+    	$params = array('action'=>'index', 'controller'=>'register', 'module'=>'user');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->getRequest()
+            ->setMethod('POST')
+            ->setPost(
+                array(
+                	'fname'=>'Jesse',
+                	'lname'=>'Lesperance',
+                    'email'=>'jesse@jplesperance.me',
+                    'username'=>'jlswebdev',
+                    'password'=>'Password1',
+                    'password2'=>'Password1',
+                    'dob'=>'2000-01-01'
+                )
+            );
+        $this->dispatch($url);
+        $this->assertModule($urlParams['module']);
+        $this->assertController($urlParams['controller']);
+        $this->assertAction($urlParams['action']);
+        $this->assertNotRedirect();
+        $this->assertQueryContentContains('h1', 'Create an account');
+        $this->assertQueryContentContains('li', 'The username you provided is already in use');
+        $this->assertQuery("form#register-form");
+        $this->assertQuery('ul');
+    }
+    
+    private function _insertTestUser(){
+    	$params = array('action'=>'index', 'controller'=>'register', 'module'=>'user');
+        $urlParams = $this->urlizeOptions($params);
+        $url = $this->url($urlParams);
+        $this->getRequest()
+            ->setMethod('POST')
+            ->setPost(
+                array(
+                	'fname'=>'Jesse',
+                	'lname'=>'Lesperance',
+                    'email'=>'jesse@jplesperance.com',
+                    'username'=>'jlswebdev',
+                    'password'=>'Password1',
+                    'password2'=>'Password1',
+                    'dob'=>'2000-01-01'
+                )
+            );
+        $this->dispatch($url);
+        return;
+    }
 
 
 }
