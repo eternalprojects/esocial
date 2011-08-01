522903076b309e4c876ff4ff73333945b9b6decc
jplesperance <jplesperance@453734df-fbbb-4f4f-9a73-a08973522899>
2011-08-01 04:28:51 +0000

    git-svn-id: https://svn.codespaces.com/jplwebinnov/esocial/trunk@108 453734df-fbbb-4f4f-9a73-a08973522899

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,18 +1,19 @@
 <?php
-use Zend\Application\Bootstrap as BaseBootstrap;
+namespace application;
+use \Zend\Application\Bootstrap as BaseBootstrap;
 class Bootstrap extends BaseBootstrap
 {
 	protected final function _initConfig()
     {
-        $config = new Zend\Config\Config($this->getOptions());
-        Zend\Registry::set('config', $config);
+        $config = new \Zend\Config\Config($this->getOptions());
+        \Zend\Registry::set('config', $config);
         return $config;
     }
     
     public function _initRoutes(){
-        $front = Zend\Controller\Front::getInstance();
-        $router = new Zend\Controller\Router\Rewrite();
-        $routes = new Zend\Config\Ini(APPLICATION_PATH .'/configs/routes.ini','routes');
+        $front = \Zend\Controller\Front::getInstance();
+        $router = new \Zend\Controller\Router\Rewrite();
+        $routes = new \Zend\Config\Ini(APPLICATION_PATH .'/configs/routes.ini','routes');
         $router->addConfig($routes, 'routes');
         $front->setRouter($router);
     }
