397f256865ac6959c9b743f6b556dfd88bad2d60
jplesperance <jplesperance@453734df-fbbb-4f4f-9a73-a08973522899>
2011-08-03 01:18:44 +0000

    SFSOCIAL-4
    A unit test for the index action to ensure redirection
    
    git-svn-id: https://svn.codespaces.com/jplwebinnov/esocial/trunk@137 453734df-fbbb-4f4f-9a73-a08973522899

---
+++ tests/application/modules/user/controllers/AuthControllerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+class User_RegisterControllerTest extends Zend_Test_PHPUnit_ControllerTestCase
+{
+
+    public function setUp()
+    {
+        $this->application = new Zend_Application(APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini');
+        $this->application->bootstrap();
+        $this->getFrontController()->setParam('bootstrap', $this->application->getBootstrap());
+        
+       
+    }
+    
+    public function tearDown() {
+        $this->resetRequest();
+        $this->resetResponse();
+        parent::tearDown();
+    }
+
+    public function testAuthIndexRedirect(){
+    		$this->dispatch('/user/auth/login');
+    		$this->assertModule('user');
+    		$this->assertController('auth');
+    		$this->assertAction('index');
+    		$this->assertRedirect();
+    		$this->assertRedirectTo('/');
+    }
+}
